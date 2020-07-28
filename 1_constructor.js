1915bc600c8c07a91956474e7da1b6a10cd12498
art-ste-web <art.ste.web@gmail.com>
2020-07-28 16:30:06 +0300

    finish constructor

--- 1_constructor.js
+++ 1_constructor.js
@@ -1,1 +1,26 @@
+//pattern Constructor
 //ES5
+function Server(name, ip) {
+    this.name = name
+    this.ip = ip
+}
+
+Server.prototype.getUrl = function() {
+    return `https://${this.ip}:80`
+}
+
+const aws = new Server('AWS German', '121.21.21.34')
+console.log(aws.getUrl());
+
+//ES6
+class ServerNew {
+    constructor(name, ip) {
+        this.name = name
+        this.ip = ip
+    }
+    getUrl() {
+        return `https://${this.ip}:80`
+    }
+}
+const srv = new ServerNew('AWS USA', '164.88.45.98')
+console.log(srv.getUrl());
